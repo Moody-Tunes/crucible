aaaa1c81fc5b534a2bf95474752f2b5b4b967c79
nickdibari <ndibari@fordham.edu>
2020-05-22 23:04:04 -0400

    Add tests for vote on song to load testing

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,12 @@
 import random
 
+from bs4 import BeautifulSoup
 from locust import HttpUser, task, between
 
 
 class BrowseActions(HttpUser):
+    emotions = ['MEL', 'CLM', 'HPY', 'EXC']  # TODO: Move to config?
+
     wait_time = between(5, 9)
 
     def on_start(self):
@@ -31,9 +34,43 @@ class BrowseActions(HttpUser):
 
     @task(1)
     def get_browse_playlist(self):
-        emotions = ['MEL', 'CLM', 'HPY', 'EXC']  # TODO: Move to config?
         self.client.get(
             '/tunes/browse/',
-            params={'emotion': random.choice(emotions)},
+            params={'emotion': random.choice(self.emotions)},
             name='/tunes/browse/?emotion=[emotion]',
+        )
+
+    @task(2)
+    def vote_on_song(self):
+        emotion = random.choice(self.emotions)
+
+        resp = self.client.get('/moodytunes/browse/')
+
+        # Parse CSRF token from response. Because we set the `HttpOnly`
+        # attribute on the CSRF-Token we need to pull the token value
+        # from the HTML config div like we do in the application
+        soup = BeautifulSoup(resp.content, 'html.parser')
+        csrf_token = soup.find(id='config')['data-csrf-token']
+
+        # Get a song from the browse playlist for emotion to vote on
+        resp = self.client.get(
+            '/tunes/browse/',
+            params={'emotion': random.choice(self.emotions)},
+            name='/tunes/browse/?emotion=[emotion]',
+        )
+
+        resp_data = resp.json()
+        song = random.choice(resp_data)
+
+        self.client.post(
+            '/tunes/vote/',
+            json={
+                'song_code': song['code'],
+                'emotion': emotion,
+                'vote': True
+            },
+            headers={
+                'X-CSRFToken': csrf_token,
+                'Referer': 'https://moodytunes.vm/moodytunes/browse/',
+            }
         )
